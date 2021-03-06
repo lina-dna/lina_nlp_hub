9434d2b63b5ee6a99d6c671271addbee7907da47
chee_mac <chee_mac@chee-macui-Macmini.local>
2021-03-07 02:16:17 +0900

    finish dockerize sentiment API endpoint

--- lina_nlp_hub/sentence_classification/pororo_sentiment_analysis/inferencer.py
+++ lina_nlp_hub/sentence_classification/pororo_sentiment_analysis/inferencer.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pororo import Pororo
 
 app = FastAPI()
 is_ready = False
@@ -7,6 +8,18 @@ model = None
 model = Pororo(task="sentiment", model="brainbert.base.ko.shopping", lang="ko")
 
 if model:
-	if is_ready = True
+	is_ready = True
 
+#endpoints
+@app.get("/")
+async def health():
+	if is_ready:
+		output = {'code': 200}
+	else:
+		output - {'code': 500}
+
+@app.post("/predict")
+async def predict_sentiment(text: str):
+	#return format example -> {'negative': 0.7525266408920288, 'positive': 0.2474733293056488}
+	return model(text, show_probs=True)
 
